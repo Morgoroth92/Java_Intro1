27ea90da9d519712b8ed800596b68d27b9014ad5
Claudio V. Carta <claudio.vittorio.carta@gmail.com>
2023-12-19 11:00:30 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int base = 10;
+        int altezza = 25;
+        int area = (base * altezza) / 2;
+
+        System.out.println("L'area del triangolo è: " + area);
+
+    }
+}
